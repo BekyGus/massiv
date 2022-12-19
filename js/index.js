0ce45391a67af3d50947d67742dd56d931fbb567
unknown <mamedli_kamil@mail.ru>
2022-12-20 02:04:06 +0400

    massiv

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let fn = [
+    ['kenan','farhad'],
+    ['rasul','murad'],
+    ['elmar','adil'],
+    ['akshin','farhad'],
+    ['sevindik','rafael']
+]
+
+console.log("fullname:" + fn[0][0][0] + fn[1][0][1] + fn[2][0][2] + fn[3][0][4] + fn[4][1][5] +" "+ fn[1][1][0] + fn[0][1][1] + fn[1][1][0] + fn[1][1][0] + fn[2][0][3] + fn[4][0][5] + fn[2][1][3] + fn[2][1][2] )
